f9cd3ca4c5ed74d2bad412df2930d87ad6fc86e7
devjanaprime <dev@primeacademy.io>
2017-04-24 08:48:52 -0500

    added front end scaffolding

--- public/client.js
+++ public/client.js
@@ -12,7 +12,20 @@ function addJoke(){
   // -- your code here --
 }// end addJokeButton on click
 
-function displayJokes() {
+function getJokes(){
+  console.log( 'getJokes function' );
+}//
+
+function displayJokes( allTheJokes ) {
+  // this function receives an array of joke objects
   console.log('displayJokes function');
-  // -- your code here --
-}// end displayJokes on click
+  // clear outputDiv
+  var outputDiv = $( '#outputDiv' );
+  outputDiv.empty();
+  // loop through jokes
+  for (var i = 0; i < allTheJokes.length; i++) {
+    // display each joke in the output div on the DOM
+    outputDiv.append( '<p>' + allTheJokes[i].jokeQuestion + '<p>' );
+    outputDiv.append( '<p>' + allTheJokes[i].punchLine + '<p>' );
+    outputDiv.append( '<p>~ <i>' + allTheJokes[i].whoseJoke + '</i><p>' );
+}// end displayJokes
